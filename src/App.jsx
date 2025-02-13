c5370f2ea3886361e57aa4a9d4b4723974a8e3d0
Sachin-0001 <sachin.samprit@gmail.com>
2025-02-13 10:54:51 +0530

    added about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Footer from "./components/footer";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-
+import About from "./pages/About";
 function App() {
   return (
     <div>
