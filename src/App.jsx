91b173962621745c098fa591e7b1e84998bd3d87
Arjun-1807 <arjun2005gowda@gmail.com>
2025-02-13 10:52:50 +0530

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Footer from "./components/footer";
 import Navbar from "./components/Navbar";
+import AppLayout from "./components/AppLayout";
 import Home from "./pages/Home";
 
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <div>
-      <Navbar />
-
-      <Home />
-
-      <Footer />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
