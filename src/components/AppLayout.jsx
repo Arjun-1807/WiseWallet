e8f51a0ce1959c734c2aea96ed1420e9067fa0bd
Arjun-1807 <arjun2005gowda@gmail.com>
2025-02-14 16:11:54 +0530

    deployment errors-2

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,19 +1,16 @@
 import { Outlet } from "react-router-dom";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
+import Footer from "./Footer.jsx";
+import Navbar from "./Navbar.jsx";
 
 function AppLayout() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 text-white">
-      {/* Navbar */}
       <Navbar />
 
-      {/* Main Content Section */}
       <main className="flex-1 container mx-auto px-4 py-6">
         <Outlet />
       </main>
 
-      {/* Footer */}
       <Footer />
     </div>
   );
