bb4d8428eeb5fc88f4753a0a5532cb1e72630c3b
Arjun-1807 <arjun2005gowda@gmail.com>
2025-02-14 16:15:29 +0530

    removed navbar

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer.jsx";
-import Navbar from "./Navbar.jsx";
+// import Navbar from "./Navbar.jsx";
 
 function AppLayout() {
   return (
